cee6642f1a1a46b824304a6a6957b21bfc991b2f
vladoskav <vladoskav@gmail.com>
2025-01-28 19:29:06 +0300

    add deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
   },
-
+    site: 'https://vladoskav.github.io',
+    base: '/Ekaterina-Kniazeva',
   integrations: [compressor()],
 });
