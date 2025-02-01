36192f93e64079c243f691cb423c3c746839a00d
vladoskav <vladoskav@gmail.com>
2025-02-01 18:09:13 +0300

    add more cases and texts

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,19 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/sharp',
+            config: {
+                limitInputPixels: false,
+            },
+        },
+    },
     site: 'https://vladoskav.github.io',
     base: '/Ekaterina-Kniazeva',
     experimental: {
         svg: true,
+        responsiveImages: true
     },
     integrations: [compressor()],
 });
--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -8,24 +8,37 @@ export const languages = {
 export const config = {
     en: {
         'name': 'Ekaterina Kniazeva',
-        'about': 'Product designer with over 3 years of experience  in mobile and web products.',
-        'subAbout': 'I currently work  in the Avito Fintech cluster.',
+        'about': 'Product designer with over 3 years of experience in mobile and web products.',
+        'subAbout': 'I currently work in the Avito Fintech cluster.',
         'resume': 'CV',
         'tg': 'Telegram',
-        'firstCaseDescr': 'NDA project. Integration with a partner who already has  a ' +
-            'financial product license to measure demand for financial products and test using an A/B entry point.',
-        'firstPosition': 'Product Designer, Avito Fintech, 2024',
-        'secondCaseDescr': 'Complete service redesign for the Billing team. The new service consists of three blocks — all services, service details and service creation with a step-by-step form.'
+        'firstCaseDescr': 'NDA. Crafting the target vision for Avito Card:' +
+            ' from transaction history and QR code payments to card reissuance and freezing.',
+        'avitoPosition': 'Product Designer, Avito Fintech, 2024',
+        'secondCaseDescr': 'NDA. A service for the Billing team.' +
+            ' The new service comprises several modules, including an all-services overview,' +
+            ' service details, and a service creation' +
+            ' feature with a step-by-step form.',
+        'thirdCaseDescr': 'NDA. A critical ' +
+            'project for the Central Bank aimed at obtaining regulatory approval.' +
+            ' Collaborated closely with legal experts and editors to ensure compliance and accuracy throughout the process.',
+        'fourthCaseDescr': 'NDA. Partner integration for measuring demand for financial products ' +
+            'and testing entry points using A/B testing.'
     },
     ru: {
         'name': 'Екатерина Князева',
-        'about': 'Продуктовый дизайнер в авито с опытом более 3 лет.',
-        'subAbout': 'На данный момент я работаю в Авито Финтех.',
+        'about': 'Продуктовый дизайнер с опытом более 3 лет в создании мобильных и веб-продуктов.',
+        'subAbout': 'На данный момент я работаю в Авито Финтех.',
         'resume': 'Резюме',
         'tg': 'Телеграм',
-        'firstCaseDescr': 'NDA project. Integration with a partner who already has  a ' +
-            'financial product license to measure demand for financial products and test using an A/B entry point.',
-        'firstPosition': 'Product Designer, Avito Fintech, 2024',
-        'secondCaseDescr': 'Complete service redesign for the Billing team. The new service consists of three blocks — all services, service details and service creation with a step-by-step form.'
-    },
+        'firstCaseDescr': 'Проект под NDA. Формирование целевого вижена Авито Карты: от истории операций,' +
+            ' оплаты по QR-коду до перевыпуска и заморозки.',
+        'avitoPosition': 'Продуктовый дизайнер, Avito Fintech, 2024',
+        'secondCaseDescr': 'Проект под NDA. Cервиc для команды Биллинга.' +
+            ' Новый сервис состоит из блоков — все услуги, детали услуги и создание услуги с пошаговой формой.',
+        'thirdCaseDescr': 'Проект под NDA.' +
+            ' Важный проект для Центрального банка для получения разрешения. Работа в связке с юристами, редакторами.',
+        'fourthCaseDescr': 'Проект под NDA. ' +
+            'Интеграция с партнёром для измерения спроса на финансовые продукты и тестирования точек входа с помощью а/б.'
+    }
 } as const;
